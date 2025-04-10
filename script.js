4dbb7ce6dbd5cef98a50a52ef526d2216c5dd911
Eren <erencoban1997@gmail.com>
2025-04-10 09:25:07 +0300

    projenin bitmeye yakin hali

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const displayMessage = function(message) {
 };
 
 document.querySelector('.check').addEventListener('click', function(){
+
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
     console.log(secretNumber,typeof secretNumber);
@@ -26,6 +27,42 @@ document.querySelector('.check').addEventListener('click', function(){
 
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+
+
+        if (score > highscore){
+            highscore = score;
+
+            document.querySelector('.highscore').textContent = highscore;
+        }
     }
 
+    //sayilar birbiriyle eslesmiyor ise
+
+
+    else if (guess !== secretNumber){
+        if (score > 1){
+            displayMessage(guess>secretNumber ? ':cok yuksek':'cok dusuk');
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            displayMessage('oyunu kaybettin');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
 });
+
+
+document.querySelector('.again').addEventListener('click', function(){
+
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    displayMessage('tahmin ediliyor');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+})
+
